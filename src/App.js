0b9da9951e14e7bf8ddca1adaf00bf3a64b64853
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-07-28 01:55:16 -0400

    tparticles bug fix

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Header from "./components/Header";
 
 function App() {
   return (
-  <>
-  <Particles/>
- <Navbar />
- <Header />
- </>
+ <div>
+<Particles/>
+<Navbar />
+<Header />
+ </div>
   );
 }
 
